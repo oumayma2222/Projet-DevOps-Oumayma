8d8053617088e07eb3d0d4b9321a71bb440faa3d
Oumayma Elbouzidi <elbouzidioumayma2023@gmail.com>
2026-01-11 20:10:22 +0100

    Simple Java app with DevOps message

---
+++ hello-devops/src/main/java/com/devops/App.java
@@ -0,0 +1,11 @@
+package com.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+    }
+}
